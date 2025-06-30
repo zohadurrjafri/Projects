07d1a39e3eab877655998072eb006f4823873b70
zohadurrjafri <zohadurrjafri@gmail.com>
2025-06-30 14:40:36 +0530

    Created every component in single page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <center className="todo-container" >
+      <h1>
+        ToDo App
+      </h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter your task here "/>
+          </div>
+          <div class="col-4">
+            <input type="date" name="" id="" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            Buy Milk
+          </div>
+          <div class="col-4">
+            22/04/2024
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            Go to College
+          </div>
+          <div class="col-4">
+            07/01/2025
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </center> 
   )
 }
 
